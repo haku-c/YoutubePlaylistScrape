7a730be4144e9fc94fb5d7e1027a8243af0399a8
Huy Nguyen <kokoanimal@gmail.com>
2023-11-18 17:01:07 -0500

    Fixes to spotify query function for single request

--- spotifyQuery/query.js
+++ spotifyQuery/query.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
 const axios = require('axios');
 const qs = require('qs');
+require('dotenv').config();
 
-const clientId = process.env.SPOTIFY_CLIENT_ID
-const clientSecret = process.env.SPOTIFY_SECRET_ID
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
 const authToken = Buffer.from(`${clientId}:${clientSecret}`, 'utf-8').toString('base64');
 
@@ -11,7 +11,7 @@ const getToken = async () => {
   try {
     //make post request to SPOTIFY API for access token, sending relavent info
     const tokenUrl = 'https://accounts.spotify.com/api/token';
-    const data = qs.q({ 'grant_type': 'client_credentials' });
+    const data = qs.stringify({ 'grant_type': 'client_credentials' });
 
     const response = await axios.post(tokenUrl, data, {
       headers: {
@@ -30,12 +30,29 @@ const getSongIds = async (q) => {
   const token = await getToken()
   const response = await axios({
     method: 'get',
-    baseUrl: 'https://api.spotify.com/v1/search?q',
-    url: q,
+    url: 'https://api.spotify.com/v1/search?q=' + q,
     headers: {
-      'Authorization': token,
+      'Authorization': `Bearer ${token}`
     }
   }).then((result) => {
+    spotifyResponse = result.data
+    let songId = spotifyResponse.tracks.items[0].id
+    let songName = spotifyResponse.tracks.items[0].name
+    let album = spotifyResponse.tracks.items[0].album.name
+    let albumId = spotifyResponse.tracks.items[0].album.id
+    let duration = spotifyResponse.tracks.items[0].duration_ms
+    let release = spotifyResponse.tracks.items[0].album.release_date
+    let artists = []
+    let artistId = []
+    artistData = spotifyResponse.tracks.items[0].artists
+    for (let i = 0; i < artistData.length; i++) {
+      artists.push(artistData[i].name)
+      console.log(artistData[i].name)
+
+    }
+    console.log(songName + ", " + album + ", " + artists[0])
 
   })
-} 
+}
+
+getSongIds('q%3Dtrack%3AInto%20Youartist%3AAriana%20Grande%20&type=track&limit=3')
--- spotifyQuery/readQueries.js
+++ spotifyQuery/readQueries.js
@@ -1,10 +1,12 @@
-var fs = require('fs');
+const fs = require('fs');
+const csvParser = require("csv-parser");
 
-var data = fs.readFileSync('queries.csv')
-  .toString() // convert Buffer to string
-  .split('\n') // split string to lines
-  .map(e => e.trim()) // remove white spaces for each line
-  .map(e => e.split(',').map(e => e.trim())); // split each line to array
-
-console.log(data);
-console.log(JSON.stringify(data, '', 2));
+var result = [];
+fs.createReadStream("./queries.csv")
+  .pipe(csvParser())
+  .on("data", (data) => {
+    result.push(data);
+  })
+  .on("end", () => {
+    console.log(result[0]);
+  });
